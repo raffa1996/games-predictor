e0d9ac5808d967f7c668ecd7b4e0f313b9c5be1d
rahul sharma tl1 <rahulsharma.tl1@oyorooms.local>
2019-09-03 15:04:30 +0530

    Updates

---
+++ precache-manifest.b51f9ed2338be159f169f066e5a246b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9f06306ba6f8fb67aa097a9ad15e55c",
+    "url": "/games-predictor/index.html"
+  },
+  {
+    "revision": "7b4e935f0a2b8392769b",
+    "url": "/games-predictor/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "5b3220c320d1e859cefd",
+    "url": "/games-predictor/static/js/2.4c22d8bc.chunk.js"
+  },
+  {
+    "revision": "7b4e935f0a2b8392769b",
+    "url": "/games-predictor/static/js/main.9b1981c6.chunk.js"
+  },
+  {
+    "revision": "9a60c2e56a4ea388a43f",
+    "url": "/games-predictor/static/js/runtime~main.209b8956.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/games-predictor/static/media/logo.5d5d9eef.svg"
+  }
+]);
